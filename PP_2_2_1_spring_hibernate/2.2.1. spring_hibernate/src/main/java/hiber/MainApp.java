137f837598218aca9365080dac384a0d7c3c549a
Fedya <160365727+itsnotfedya@users.noreply.github.com>
2024-04-19 02:26:11 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,56 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car car1 = new Car("Nissan Fairlady Z", 1999);
+      Car car2 = new Car("Mazda RX-7 Veilside", 1997);
+      Car car3 = new Car("Nissan Skyline GT-R R33", 1996);
+      Car car4 = new Car("Mitsubishi Lancer Evolution IX", 2006);
+
+      user1.setCar(car4);
+      car1.setUser(user1);
+
+      user2.setCar(car3);
+      car3.setUser(user2);
+
+      user3.setCar(car2);
+      car2.setUser(user3);
+
+      user4.setCar(car1);
+      car1.setUser(user4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println(user);
+      }
+
+      User driver = userService.findUserByHisCar(car4);
+      System.out.println("\nЭту машину водит: " + driver);
+
+      context.close();
+   }
+}
---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,37 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User findUserByHisCar(Car car) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User where car=:car", User.class);
+        query.setParameter("car", car);
+        List<User> users = query.getResultList();
+        return users.isEmpty() ? null : users.get(0);
+    }
+}
